2705453acc431c06df8110768ec634736b3706fb
fmatzy <30208309+fmatzy@users.noreply.github.com>
2018-09-17 11:32:06 +0900

    use filters instead of methods

--- components/Post.vue
+++ components/Post.vue
@@ -1,7 +1,7 @@
 <template>
   <li class="post">
     <span class="title"><a :href="post.url">{{post.title}}</a></span>
-    <span class="created">{{ formatTime(post.created_at) }}</span>
+    <span class="created">{{ post.created_at | formatTime }}</span>
   </li>
 </template>
 
@@ -13,12 +13,15 @@ import {
 } from "nuxt-property-decorator"
 import moment from "moment"
 
-@Component({})
+@Component({
+  filters: {
+    formatTime: function(time: string): string {
+      return moment(time).format('MMMM Do YYYY, h:mm:ss')
+    }
+  }
+})
 export default class Post extends Vue {
   @Prop() post
-  formatTime(time: string): string {
-    return moment(time).format('MMMM Do YYYY, h:mm:ss')
-  }
 }
 </script>
 
